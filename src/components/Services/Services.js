8bf8bae404f2521663be105bea9d3e55515a3306
omkar2809 <zemseom10@gmail.com>
2021-08-04 13:19:52 +0530

    warnings

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -15,7 +15,7 @@ const Services = () => {
                     <div className="item features-image сol-12 col-md-6 col-lg-4">
                         <div className="item-wrapper">
                             <div className="item-img">
-                                <img src={features1} />
+                                <img src={features1} alt="" />
                             </div>
                             <div className="item-content">
                                 <h5 className="item-title mbr-fonts-style display-7"><strong>No Coding</strong></h5>
@@ -23,7 +23,7 @@ const Services = () => {
                                 <p className="mbr-text mbr-fonts-style mt-3 display-7">Mobirise is an easy website builder. Just
                                     drop site elements to your page, add content and style it to look the way you like.</p>
                             </div>
-                            <div className="mbr-section-btn item-footer mt-2"><a href="" className="btn btn-primary item-btn display-7" target="_blank">Learn More
+                            <div className="mbr-section-btn item-footer mt-2"><a href="www.google.in" className="btn btn-primary item-btn display-7">Learn More
                                     </a></div>
                         </div>
                     </div>
@@ -31,7 +31,7 @@ const Services = () => {
                     <div className="item features-image сol-12 col-md-6 col-lg-4">
                         <div className="item-wrapper">
                             <div className="item-img">
-                                <img src={features2}/>
+                                <img src={features2} alt=""/>
                             </div>
                             <div className="item-content">
                                 <h5 className="item-title mbr-fonts-style display-7"><strong>Mobile Friendly</strong></h5>
@@ -39,7 +39,7 @@ const Services = () => {
                                 <p className="mbr-text mbr-fonts-style mt-3 display-7">You don't have to code to create your own
                                     site. Select one of available themes in the Mobirise Site Maker.</p>
                             </div>
-                            <div className="mbr-section-btn item-footer mt-2"><a href="" className="btn btn-primary item-btn display-7" target="_blank">Learn More
+                            <div className="mbr-section-btn item-footer mt-2"><a href="www.google.in" className="btn btn-primary item-btn display-7">Learn More
                                     &gt;</a></div>
                         </div>
                     </div>
@@ -47,7 +47,7 @@ const Services = () => {
                     <div className="item features-image сol-12 col-md-6 col-lg-4">
                         <div className="item-wrapper">
                             <div className="item-img">
-                                <img src={features3}/>
+                                <img src={features3} alt=""/>
                             </div>
                             <div className="item-content">
                                 <h5 className="item-title mbr-fonts-style display-7"><strong>Unique Styles</strong></h5>
@@ -55,7 +55,7 @@ const Services = () => {
                                 <p className="mbr-text mbr-fonts-style mt-3 display-7">Select the theme that suits you. Each theme
                                     in the Mobirise Website Software contains a set of unique blocks.<br /></p>
                             </div>
-                            <div className="mbr-section-btn item-footer mt-2"><a href="" className="btn btn-primary item-btn display-7" target="_blank">Learn More
+                            <div className="mbr-section-btn item-footer mt-2"><a href="www.google.in" className="btn btn-primary item-btn display-7">Learn More
                                     &gt;</a></div>
                         </div>
                     </div>
