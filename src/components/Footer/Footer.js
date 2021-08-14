e5ad1674a15a4ef44c38c6fdb0f9e9088307556d
Ganesh-11062000 <ganeshnm2000@gmail.com>
2021-08-14 14:16:58 +0530

    changed the footer UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,18 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <section class="footer6 cid-sEELpHV0RX" once="footers" id="footer6-12">
+            <section class="footer6 cid-sEELpHV0RX" once="footers" id="footer6-12" style={{'backgroundColor':'#91ACBC'}}>
                 <div class="container">
                     <div class="row content mbr-white">
-                        <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                        <div class="col-12 col-md-6 mbr-fonts-style display-7">
                             <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7">
                                 <strong>Address</strong>
                             </h5>
                             <p class="mbr-text mbr-fonts-style display-7">
-                                1234 Street Name <br/>
-                                City, AA 99999
+                            Pandurang Budhkar Marg Mahalaxmi SRA, <br/>
+                            CHSL I - Wing, <br/>
+                            Shop No1, Worli, <br/>
+                            Maharashtra 400030, India 
                             </p> 
                             <br />
                             <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 mt-4 display-7">
@@ -24,20 +26,18 @@ const Footer = () => {
                                 Fax: +91 000 0000 002
                             </p>
                         </div>
-                        <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                        {/* <div class="col-12 col-md-3 mbr-fonts-style display-7">
                             <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-7">
                                 <strong>Links</strong>
                             </h5>
                             <ul class="list mbr-fonts-style mb-4 display-4">
                                 <li class="mbr-text item-wrap">
-                                    <a class="text-primary" href="https://mobirise.co/">Link 1</a>
+                                    <a class="text-primary" href="https://gmp-construction.netlify.app/">https://gmp-construction.netlify.app/</a>
                                 </li>
                                 <li class="mbr-text item-wrap">
-                                    <a class="text-primary" href="https://mobirise.co/">Link 2</a>
+                                    <a class="text-primary" href="https://gmp-construction.netlify.app/">https://gmp-construction.netlify.app/</a>
                                 </li>
-                                <li class="mbr-text item-wrap">
-                                    <a class="text-primary" href="https://mobirise.co/">Link 3</a>
-                                </li>
+                                
                             </ul>
                             <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 mt-5 display-7">
                                 <strong>Feedback</strong>
@@ -45,13 +45,13 @@ const Footer = () => {
                             <p class="mbr-text mbr-fonts-style mb-4 display-7">
                                 Please send us your ideas, bug reports, suggestions! Any feedback would be appreciated.
                             </p>
-                        </div>
+                        </div> */}
                         <div class="col-12 col-md-6">
                             <div class="google-map">
                                 <iframe title="one" frameborder="0" style={{border:0}} src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDk89J4FSunMF33ruMVWJaJht_Ro0kvoXs&amp;q=350 5th Ave, New York, NY 10118" allowfullscreen=""></iframe>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        {/* <div class="col-md-6">
                             <div class="social-list align-left">
                                 <div class="soc-item">
                                     <a href="https://twitter.com/mobirise">
@@ -80,14 +80,44 @@ const Footer = () => {
                                 </div>
                                 
                             </div>
-                        </div>
+                        </div> */}
                     </div>
-                    <div class="footer-lower">
-                        <div class="media-container-row">
+                    <div class="col-12 p-0">
+                            <div class="social-list">
+                                <div class="soc-item">
+                                    <a href="https://twitter.com/mobirise">
+                                        <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="https://www.facebook.com/pages/Mobirise/1616226671953247">
+                                        <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="https://www.youtube.com/c/mobirise">
+                                        <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="https://instagram.com/mobirise">
+                                        <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="https://plus.google.com/u/0/+Mobirise">
+                                        <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    <div class="footer-lower ">
+                        {/* <div class="media-container-row">
                             <div class="col-sm-12">
                                 <hr/>
                             </div>
-                        </div>
+                        </div> */}
                         <div class="col-sm-12 copyright pl-0">
                             <p class="mbr-text mbr-fonts-style mbr-white display-7">
                                 © Copyright 2021 GMP Constructions - All Rights Reserved
@@ -95,12 +125,6 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-            </section>
-            <section style={{backgroundColor: '#fff', color: '#aaa', fontSize: '12px', padding: 0, alignItems: 'center', display: 'flex'}}>
-                <a href="https://mobirise.site/n" style={{flex: '1 1', height: '3rem', paddingLeft: '1rem'}}>
-                </a>
-                {/* <p style={{flex: '0 0 auto', margin: 0, paddingRight: '1rem'}}>Made with 
-                <a href="https://mobirise.site/a" style={{color:'#aaa'}}>Mobirise</a></p> */}
             </section>
         </div>
     )
