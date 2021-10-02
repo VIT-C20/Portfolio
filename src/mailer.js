1573f0aed8f24eeee274a787b38e0d53b6540d40
omkar2809 <zemseom10@gmail.com>
2021-10-02 11:40:58 +0530

    mailer

---
+++ src/mailer.js
@@ -0,0 +1,4 @@
+export const service_id = 'service_n7rotpv'
+export const user_id = 'user_UnyiyhnvfezsYTmu87jcc'
+export const contact_us_template_id = 'template_065lk7w'
+export const get_quotation_template_id = 'template_3x0wvcc'
